7fb64979b862337cd488fe9da934f73cf53402a1
Luoxin <luoxin@ly-sky.com>
2019-02-25 09:56:34 +0800

    新增測試項目

---
+++ src/main/java/com/example/demo/authc/dto/AppsDto.java
@@ -0,0 +1,40 @@
+package com.example.demo.authc.dto;
+
+public class AppsDto {
+    private String id;
+    private String name;
+    private Integer status;
+    private String keyword;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+}
